7fcd21ab2d49732a809661f9d8198ed26c396316
dentudave <dentudave@gmail.com>
2016-09-05 17:27:20 -0500

    Checking diagonally right to left and left to right should work now.

--- main.rb
+++ main.rb
@@ -43,11 +43,72 @@ def check_top_to_bottom(file_contents, word_length)
 	return words
 end
 
+def check_top_left_to_bottom_right(file_contents, word_length)
+	#treat these lines like a puzzle from left to right
+	possible_words = Array.new
+	diagonal_lines = get_diagonal_lines(file_contents)
+	for i in 0...diagonal_lines.length
+		possible_words += check_left_to_right(diagonal_lines[i], word_length)
+	end
+	return possible_words
+end
+
+#get the lines that run left to right diagonally
+def get_diagonal_lines(file_contents)
+	contents_copy = Array.new(file_contents)
+	for i in 0...contents_copy.length
+		contents_copy[i] = contents_copy[i].split('')
+	end
+
+	diagonal_lines = Array.new
+	for i in 0...contents_copy.length
+		diagonal_lines.push(take_diagonal_x_slice(contents_copy,i))
+		diagonal_lines.push(take_diagonal_y_slice(contents_copy,i))
+	end
+	#puts diagonal_lines
+	return diagonal_lines
+end
+
+def take_diagonal_x_slice(arr2d, start_x)
+	slice = Array.new
+	for x in 0...arr2d[0].length
+		unless x + start_x > arr2d[0].length
+			slice.push(arr2d[x][start_x + x])
+		end
+	end
+
+	return slice.join('')
+end
+
+def take_diagonal_y_slice(arr2d, start_y)
+	slice = Array.new
+	for y in 0...arr2d.length
+		if y + start_y < arr2d.length && y + start_y < arr2d[0].length
+			slice.push(arr2d[start_y + y][y])
+		end
+	end
+	return slice.join('')
+end
+
+def check_top_right_to_bottom_left(file_contents, word_length)
+	contents_copy = Array.new(file_contents)
+	for i in 0...contents_copy.length
+		contents_copy[i].reverse!
+	end
+
+	possible_words = Array.new
+	#since it is reversed, checking top left to bottom right is the same as checking top right to bottom left
+
+	possible_words += check_top_left_to_bottom_right(contents_copy, word_length)
+
+	return possible_words
+end
+
 def main
 	file_contents = IO.readlines('puzzle.txt')
 
 	for i in 0...file_contents.length
-		puts file_contents[i]
+		#puts file_contents[i]
 		file_contents[i] = file_contents[i].strip!
 	end
 
@@ -63,9 +124,14 @@ def main
 	# end
 	# puts file_contents
 
-	words += check_top_to_bottom(file_contents, hint_lengths[0])
+	#words += check_top_to_bottom(file_contents, hint_lengths[0])
 
-	puts words
+
+	#words += check_top_left_to_bottom_right(file_contents, hint_lengths[0])
+	#puts words
+
+	words += check_top_right_to_bottom_left(file_contents, hint_lengths[0])
+	#puts words
 end
 
 main
