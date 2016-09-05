1de3528cdd179fa6e97e6093f05549f270c6d936
dentudave <dentudave@gmail.com>
2016-09-05 07:25:11 -0500

    Got checking top to bottom for words done.

--- main.rb
+++ main.rb
@@ -13,7 +13,7 @@ require './DictionaryCheck.rb'
 def check_left_to_right(line, word_length)
 	words = Array.new
 	for i in 0...line.length
-		unless i + word_length >= line.length
+		unless i + word_length > line.length
 			possible_word = line.slice(i, word_length)
 			words.push(possible_word)
 			words.push(possible_word.reverse)
@@ -23,57 +23,47 @@ def check_left_to_right(line, word_length)
 end
 
 def check_top_to_bottom(file_contents, word_length)
+	words = Array.new
+	#copy the text into an array and subarrays
 	contents_copy = Array.new(file_contents)
-
 	for i in 0...contents_copy.length
 		contents_copy[i] = contents_copy[i].split('')
-
 	end
-	rotatedArray = file_contents.transpose
 
-	words = Array.new
-	for i in 0...rotatedArray.size
-		words += check_left_to_right(rotatedArray, hint_lengths[0])
+	#now that it's an array, we can transpose it (rotate it 90 degrees)
+	contents_copy = contents_copy.transpose
+	#now join all of the subarrays into a string again
+	for i in 0...contents_copy.length
+		contents_copy[i] = contents_copy[i].join('')
+	end
+	#since it is rotated, checking left to right is the same as checking top to bottom
+	for i in 0...contents_copy.length
+		words += check_left_to_right(contents_copy[i], word_length)
 	end
 	return words
-
 end
-
-# def check_top_to_bottom(file_contents, word_length)
-# 	#rotatedArray = file_contents.transpose
-# 	words = Array.new
-# 	puts file_contents[0].length
-# 	for i in 0...file_contents[0].length
-# 		for j in 0...file_contents.length
-# 			unless j + word_length >= file_contents.length
-# 				word = ''
-# 				for k in 0...word_length
-# 					word += file_contents[k]
-# 					#puts word
-# 				end
-# 				puts word
-# 			end
-# 		end
-
-# 	end
-
-
-# end
 
 def main
 	file_contents = IO.readlines('puzzle.txt')
+
+	for i in 0...file_contents.length
+		puts file_contents[i]
+		file_contents[i] = file_contents[i].strip!
+	end
+
 
 	hint_lengths = [ 4, 5, 8, 10 ]
 
 	dictionary = DictionaryCheck.new
 
 	words = Array.new
+	#go through the wordSearch left to right, finding words
 	# for i in 0...file_contents.size
 	# 	words += check_left_to_right(file_contents[i], hint_lengths[0])
 	# end
+	# puts file_contents
 
 	words += check_top_to_bottom(file_contents, hint_lengths[0])
-
 
 	puts words
 end
